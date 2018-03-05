3512434869bdbb91e3fd52e181771d9b5f674db8
John Hancock <john@dev.jhnhnck.com>
2018-03-05 00:39:37 -0500

    Optimized out default value calls

--- generate_code.py
+++ generate_code.py
@@ -2,7 +2,8 @@ import json
 import os
 from datetime import datetime
 
-template_before = """\
+template = {
+  'before': """\
 /**
  * ###################################################
  * ### DO NOT EDIT --- THIS CODE IS AUTO GENERATED ###
@@ -16,28 +17,57 @@ template_before = """\
  *  - John Hancock (@jhnhnck)
  */
 
+#include "API.h"
 #include "constants.h"
 #include "motors.h"
-#include "API.h"
 
-void auton_{}(Encoder le, Encode re) {{
-"""
+void auton_{}(Encoder le, Encode re) {{""",
+  'breakpoint': [
+    '\n  /**',
+    '   *  ______     ______     ______     ______     __  __     ______   ______     __     __   __     ______',
+    '   * /\  == \   /\  == \   /\  ___\   /\  __ \   /\ \/ /    /\  == \ /\  __ \   /\ \   /\ "-.\ \   /\__  _\\',
+    '   * \ \  __<   \ \  __<   \ \  __\   \ \  __ \  \ \  _"-.  \ \  _-/ \ \ \/\ \  \ \ \  \ \ \-.  \  \/_/\ \/',
+    '   *  \ \_____\  \ \_\ \_\  \ \_____\  \ \_\ \_\  \ \_\ \_\  \ \_\    \ \_____\  \ \_\  \ \_\\\\"\_\    \ \_\\',
+    '   *   \/_____/   \/_/ /_/   \/_____/   \/_/\/_/   \/_/\/_/   \/_/     \/_____/   \/_/   \/_/ \/_/     \/_/',
+    '   *',
+    '   */',
+  ],
+  'comment': '\n  // {} {} {}',
+  'functions': [
+    '  setVelocityLY({});',
+    '  setVelocityRY({});',
+    '  setVelocityDR4B({});',
+    '  setVelocityMogo({});',
+    '  setVelocityFourBar({});',
+    '  setVelocityRoller({});',
+  ],
+  'section_end': [
+    '  doMotorTick();',
+    '  delay(AUTON_LOOP_DELAY);',
+  ],
+  'section_delay': [
+    '\n  // Condenensed {} empty cycles',
+    '  doMotorTick();',
+    '  delay(AUTON_LOOP_DELAY * {});',
+  ],
+  'section_delay_end': [
+    '\n  // Recording ended with {} empty cycles (Line added below if needed)',
+    '  // delay(AUTON_LOOP_DELAY * {});',
+  ],
+  'after': '}\n'
+}
 
-template_line ="""\
-  // {} {} {}
-  setVelocityLY({});
-  setVelocityRY({});
-  setVelocityDR4B({});
-  setVelocityMogo({});
-  setVelocityFourBar({});
-  setVelocityRoller({});
+speed_defaults = [0, 0, 0, 0, 0, 30]
 
-  doMotorTick();
-  delay(AUTON_LOOP_DELAY);
+def unpack(packet):
+  for key, dataset in packet.items():
+    if len(dataset[0]) != 6:
+      raise Exception('Invalid array length for speeds: {}',format(dataset[0]))
 
-"""
+    if len(dataset[1]) != 4:
+      raise Exception('Invalid array length for sensors:  {}',format(dataset[1]))
 
-template_after = '}'
+    return key, dataset[0], dataset[1]
 
 
 def generate_file(basename):
@@ -50,17 +80,54 @@ def generate_file(basename):
 
   with open(in_filename) as in_file:
     with open(out_filename, 'w') as out_file:
-      out_file.write(template_before.format(
-        record_date,
-        basename))
+      zero_count = 0
+
+      out_file.write(template.get('before').format(record_date, basename))
+
       for line in in_file:
         try:
           packet = json.loads(line)
-          for key, value in packet.items():
-            out_file.write(template_line.format(key, value[0], value[1], *(value[0])))
+
+          if packet.get('break', False):
+            for line in template.get('breakpoint'):
+              out_file.write('{}\n'.format(line))
+            continue
+
+          cycle, speed_set, sensor_set = unpack(packet)
+          setters = []
+          write_lines = []
+
+          for i in range(0, 6):
+            if speed_set[i] != speed_defaults[i]:
+              setters.append(template.get('functions')[i].format(speed_set[i]))
+
+
+          if len(setters) != 0:
+            if zero_count > 0:
+              for line in template.get('section_delay'):
+                write_lines.append(line.format(zero_count))
+
+            write_lines.append(template.get('comment').format(cycle, speed_set, sensor_set))
+            write_lines.extend(setters)
+            write_lines.extend(template.get('section_end'))
+
+            zero_count = 0
+          else:
+            zero_count += 1
+
+          for line in write_lines:
+            out_file.write('{}\n'.format(line))
+
         except Exception as e:
-          print ('Error on Line {}: {}\n{}'.format(out_file.tell(), e, line))
-      out_file.write(template_after)
+          print('Error on Line {}: {}\n{}'.format(out_file.tell(), e, line))
+          continue
+
+
+      if zero_count > 0:
+        for line in template.get('section_delay'):
+          out_file.write('{}\n'.format(line.format(zero_count)))
+
+      out_file.write(template.get('after'))
 
 
 if __name__ == "__main__":
